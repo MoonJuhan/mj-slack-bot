e409ec7af864622bbabd577783bd81b38a1d434c
MoonJuhan <codemike@naver.com>
2021-08-18 16:40:33 +0900

    test: JSON 반환 api 테스트

--- api/index.js
+++ api/index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express()
 
 app.get('/api', (req, res) => {
-    const path = `PATH`;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a>${path}</a>`);
-  });
+  const path = `PATH`
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  res.end(`Hello! Go to item: <a>${path}</a>`)
+})
 
-module.exports = app;
+app.get('/api/test', (req, res) => {
+  res.setHeader('Content-Type', 'Application/json')
+  const testJSON = {
+    key: "value"
+  }
+  res.end(testJSON)
+})
+
+
+
+module.exports = app
