d93c45a96b315b8effda175c71acad485da844b5
MoonJuhan <codemike@naver.com>
2021-08-18 17:00:40 +0900

    test: 슬랙 api 테스트

--- api/index.js
+++ api/index.js
@@ -1,6 +1,33 @@
 const express = require('express')
 const app = express()
 
+const Slack = require('slack-node')
+
+// 생성한 token
+const API_TOKEN = process.env.token
+
+const slack = new Slack(API_TOKEN)
+
+const send = async (sender, message) => {
+  slack.api(
+    'chat.postMessage',
+    {
+      text: `${sender}:\n${message}`,
+      channel: '#general',
+      icon_emoji: 'slack',
+    },
+    (error, response) => {
+      if (error) {
+        console.log(error)
+        return
+      }
+      console.log(response)
+    }
+  )
+}
+
+
+
 app.get('/api', (req, res) => {
   const path = `PATH`
   res.setHeader('Content-Type', 'text/html')
@@ -8,14 +35,14 @@ app.get('/api', (req, res) => {
   res.end(`Hello! Go to item: <a>${path}</a>`)
 })
 
-app.get('/api/test', (req, res) => {
-  res.setHeader('Content-Type', 'Application/json')
+app.get('/api/test', async (req, res) => {
+  await send('user1', 'send message')
+
+  res.setHeader('Content-Type', 'application/json')
   const testJSON = {
-    key: "value"
+    key: 'value',
   }
-  res.end(testJSON)
+  res.json(testJSON)
 })
-
-
 
 module.exports = app
