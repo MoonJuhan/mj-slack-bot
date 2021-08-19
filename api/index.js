b004d68ebd102ce93d7203c2d42e88329735558f
MoonJuhan <codemike@naver.com>
2021-08-19 10:21:19 +0900

    test: 로그 추가

--- api/index.js
+++ api/index.js
@@ -9,6 +9,7 @@ const API_TOKEN = process.env.TOKEN
 const web = new WebClient(API_TOKEN)
 
 app.get('/api', (req, res) => {
+  console.log('API Call')
   const path = `PATH`
   res.setHeader('Content-Type', 'text/html')
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
@@ -16,16 +17,17 @@ app.get('/api', (req, res) => {
 })
 
 app.post('/api/slack/event', (req, res) => {
-  const body = req.body
-  const event = body.event
+  console.log('API POST Call')
+  console.log(req.body)
 
-  web.chat
-    .postMessage({ channel: event.channel, text: '안녕하세요.' })
-    .then((result) => {
-      console.log('Message sent: ' + result.ts)
-    })
+  // web.chat
+  //   .postMessage({ channel: event.channel, text: '안녕하세요.' })
+  //   .then((result) => {
+  //     console.log('Message sent: ' + result.ts)
+  //   })
 
-  res.json({challenge: body.challenge})
+  // res.json({challenge: body.challenge})
+  res.json({challenge:'hello'})
 })
 
 module.exports = app
