1ded4ac2d0318e965285a816ae0b7c0a684f7276
MoonJuhan <codemike@naver.com>
2021-11-04 16:37:12 +0900

    fix: message 일반 답변 프리픽스 삭제

--- api/message.js
+++ api/message.js
@@ -3,7 +3,7 @@ const messages = require('./messages.json')
 const getMessage = (key) => {
   return typeof messages[key] === 'string'
     ? messages[key]
-    : messages[key].map((el) => 'MJ Slack Bot Talking \n' + el)[Math.ceil(Math.random() * messages[key].length) - 1]
+    : messages[key][Math.ceil(Math.random() * messages[key].length) - 1]
 }
 
 module.exports = getMessage
